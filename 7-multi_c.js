3486bfd2fe97ded52e83eb81ab46aac9d6573a3a
Eslam Al-Okda <al3kda1999@gmail.com>
2025-11-16 03:50:15 +0200

    Edit answer at '7-multi_c.js' file

--- 7-multi_c.js
+++ 7-multi_c.js
@@ -3,9 +3,11 @@ const x = Number(process.argv[2]);
 if (isNaN(x)) {
     console.log("Missing number of occurrences");
 } else {
-    let output = "";
-    for (let i = 0; i < x; i++) {
-        output += "C is fun\n";
+    if (x > 0) {
+        let output = "";
+        for (let i = 0; i < x; i++) {
+            output += "C is fun\n";
+        }
+        console.log(output.trim());
     }
-    console.log(output.trim());
 }
